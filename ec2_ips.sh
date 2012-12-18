346d582c770deec333ec1ce38a87fed8e59148ca
Ngoc Dao <ngocdaothanh@gmail.com>
2012-12-18 11:18:50 +0900

    http://about.silkapp.com/page/Multiple%20IP%20addresses%20on%20Amazon%20EC2

--- ec2_ips.sh
+++ ec2_ips.sh
@@ -1,3 +1,4 @@
+# http://about.silkapp.com/page/Multiple%20IP%20addresses%20on%20Amazon%20EC2
 MAC_ADDR=$(ifconfig eth0 | sed -n 's/.*HWaddr \([a-f0-9:]*\).*/\1/p')
 IP=($(curl http://169.254.169.254/latest/meta-data/network/interfaces/macs/$MAC_ADDR/local-ipv4s))
 for ip in ${IP[@]:1}; do
